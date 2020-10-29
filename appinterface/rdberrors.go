651aca201b49153d63a3607e61ca0388c8f33963
Calvin Lau <calvinlauco@gmail.com>
2020-10-30 00:59:50 +0800

    Address review comments in #9

--- appinterface/rdberrors.go
+++ appinterface/rdberrors.go
@@ -1,10 +1,19 @@
 package appinterface
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrRepoPrepare = errors.New("error preparing repository operation")
 	ErrRepoOpen    = errors.New("error opening repository")
 	ErrRepoQuery   = errors.New("error querying from repository")
 	ErrRepoWrite   = errors.New("error writing to repository")
+
+	ErrBuildSQLStmt = fmt.Errorf("error building SQL statement: %w", ErrRepoPrepare)
+	ErrTypeConv     = fmt.Errorf("error converting between types: %w", ErrRepoPrepare)
+
+	// when trying to scan a null row
+	ErrNoRows = errors.New("no rows in result set")
 )
