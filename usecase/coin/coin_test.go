8a6eb2108fc2ae9e52aa5f4ccfe4159f97df5629
Calvin Lau <calvinlauco@gmail.com>
2020-11-14 02:18:44 +0800

    Refactor coin test

--- usecase/coin/coin_test.go
+++ usecase/coin/coin_test.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"math/big"
 
-	coin2 "github.com/crypto-com/chainindex/usecase/coin"
+	coin "github.com/crypto-com/chainindex/usecase/coin"
 
 	jsoniter "github.com/json-iterator/go"
 	. "github.com/onsi/ginkgo"
@@ -16,48 +16,48 @@ var _ = Describe("Coin", func() {
 	Describe("NewCoin", func() {
 		It("should return Error when the big.Int pointer is nil", func() {
 			var anyNilBigInt *big.Int
-			_, err := coin2.NewCoin(anyNilBigInt)
+			_, err := coin.NewCoin(anyNilBigInt)
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinNil)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinNil)).To(BeTrue())
 		})
 	})
 
 	Describe("NewCoinFromString", func() {
 		It("should return Error when the string is invalid coin", func() {
-			_, err := coin2.NewCoinFromString("invalid")
+			_, err := coin.NewCoinFromString("invalid")
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinInvalid)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinInvalid)).To(BeTrue())
 		})
 
 		It("should return Error when the string has decimal place", func() {
-			_, err := coin2.NewCoinFromString("1.12345678")
+			_, err := coin.NewCoinFromString("1.12345678")
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinInvalid)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinInvalid)).To(BeTrue())
 
 		})
 
 		It("should return Error when the string exceeds maximum supply", func() {
 			exceedTotalSupply := "10000000000000000001"
-			_, err := coin2.NewCoinFromString(exceedTotalSupply)
+			_, err := coin.NewCoinFromString(exceedTotalSupply)
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinExceedSupply)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinExceedSupply)).To(BeTrue())
 		})
 
 		It("should return Error when the string negatively exceeds maximum supply", func() {
 			exceedTotalSupply := "-10000000000000000001"
-			_, err := coin2.NewCoinFromString(exceedTotalSupply)
+			_, err := coin.NewCoinFromString(exceedTotalSupply)
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinExceedSupply)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinExceedSupply)).To(BeTrue())
 		})
 
 		It("should return the Coin representation of the string", func() {
 			anyCoin := "100000000"
-			coin, err := coin2.NewCoinFromString(anyCoin)
+			coin, err := coin.NewCoinFromString(anyCoin)
 
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("100000000"))
@@ -66,87 +66,87 @@ var _ = Describe("Coin", func() {
 
 	Describe("NewCoinFromCRO", func() {
 		It("should return Error when the CRO has more than 8 decimal places", func() {
-			_, err := coin2.NewCoinFromCRO("1.123456789")
+			_, err := coin.NewCoinFromCRO("1.123456789")
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinInvalid)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinInvalid)).To(BeTrue())
 		})
 
 		It("should return Error when the CRO exceed maximum supply", func() {
-			_, err := coin2.NewCoinFromCRO("100000000000.1")
+			_, err := coin.NewCoinFromCRO("100000000000.1")
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinExceedSupply)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinExceedSupply)).To(BeTrue())
 		})
 
 		It("should return Error when the CRO negatively exceed maximum supply", func() {
-			_, err := coin2.NewCoinFromCRO("-100000000000.1")
+			_, err := coin.NewCoinFromCRO("-100000000000.1")
 
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinExceedSupply)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinExceedSupply)).To(BeTrue())
 		})
 
 		It("should return the Coin representation of a negative CRO", func() {
-			var coin coin2.Coin
+			var coin coin.Coin
 			var err error
 
-			coin, err = coin2.NewCoinFromCRO("-99999999999.09999999")
+			coin, err = coin.NewCoinFromCRO("-99999999999.09999999")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("-9999999999909999999"))
 
-			coin, err = coin2.NewCoinFromCRO("-12345678")
+			coin, err = coin.NewCoinFromCRO("-12345678")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("-1234567800000000"))
 
-			coin, err = coin2.NewCoinFromCRO("-0.00000884")
+			coin, err = coin.NewCoinFromCRO("-0.00000884")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("-884"))
 		})
 
 		It("should return the Coin representation of a CRO integer", func() {
-			coin, err := coin2.NewCoinFromCRO("12345678")
+			coin, err := coin.NewCoinFromCRO("12345678")
 
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("1234567800000000"))
 		})
 
 		It("should return the Coin representation of the CRO", func() {
-			var coin coin2.Coin
+			var coin coin.Coin
 			var err error
 
-			coin, err = coin2.NewCoinFromCRO("0.12345678")
+			coin, err = coin.NewCoinFromCRO("0.12345678")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("12345678"))
 
-			coin, err = coin2.NewCoinFromCRO("0.00000884")
+			coin, err = coin.NewCoinFromCRO("0.00000884")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("884"))
 
-			coin, err = coin2.NewCoinFromCRO("0.00088400")
+			coin, err = coin.NewCoinFromCRO("0.00088400")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("88400"))
 		})
 
 		It("should return the Coin representation of a small CRO ", func() {
-			coin, err := coin2.NewCoinFromCRO("0.00000001")
+			coin, err := coin.NewCoinFromCRO("0.00000001")
 
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("1"))
 		})
 
 		It("should return the Coin representation of a large CRO ", func() {
-			var coin coin2.Coin
+			var coin coin.Coin
 			var err error
 
-			coin, err = coin2.NewCoinFromCRO("100000000000")
+			coin, err = coin.NewCoinFromCRO("100000000000")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("10000000000000000000"))
 
-			coin, err = coin2.NewCoinFromCRO("99999999999.09999999")
+			coin, err = coin.NewCoinFromCRO("99999999999.09999999")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("9999999999909999999"))
 
-			coin, err = coin2.NewCoinFromCRO("99999999999.00000884")
+			coin, err = coin.NewCoinFromCRO("99999999999.00000884")
 			Expect(err).To(BeNil())
 			Expect(coin.String()).To(Equal("9999999999900000884"))
 		})
@@ -155,22 +155,22 @@ var _ = Describe("Coin", func() {
 	Describe("Add", func() {
 		It("should return Error and do not change the coin value when the result exceeds total supply", func() {
 			anyAmount := "10000000000000000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			oneUnit := "1"
-			oneCoin, _ := coin2.NewCoinFromString(oneUnit)
+			oneCoin, _ := coin.NewCoinFromString(oneUnit)
 
 			_, err := coin.Add(oneCoin)
 			Expect(err).NotTo(BeNil())
-			Expect(errors.Is(err, coin2.ErrCoinExceedSupply)).To(BeTrue())
+			Expect(errors.Is(err, coin.ErrCoinExceedSupply)).To(BeTrue())
 		})
 
 		It("should return the sum of the two coin values", func() {
 			anyAmount := "10000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			oneUnit := "1"
-			oneCoin, _ := coin2.NewCoinFromString(oneUnit)
+			oneCoin, _ := coin.NewCoinFromString(oneUnit)
 
 			actual, err := coin.Add(oneCoin)
 			Expect(err).To(BeNil())
@@ -179,10 +179,10 @@ var _ = Describe("Coin", func() {
 
 		It("should not update the original coin value", func() {
 			anyAmount := "10000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			oneUnit := "1"
-			oneCoin, _ := coin2.NewCoinFromString(oneUnit)
+			oneCoin, _ := coin.NewCoinFromString(oneUnit)
 
 			_, err := coin.Add(oneCoin)
 			Expect(err).To(BeNil())
@@ -193,7 +193,7 @@ var _ = Describe("Coin", func() {
 	Describe("Negate", func() {
 		It("should negate the underlying coin value when it is positive", func() {
 			anyAmount := "10000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			actual := coin.Negate()
 			Expect(actual.String()).To(Equal("-10000000"))
@@ -201,7 +201,7 @@ var _ = Describe("Coin", func() {
 
 		It("should convert negative coin to positive", func() {
 			anyAmount := "-10000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			actual := coin.Negate()
 			Expect(actual.String()).To(Equal("10000000"))
@@ -209,7 +209,7 @@ var _ = Describe("Coin", func() {
 
 		It("should not update the original coin value", func() {
 			anyAmount := "10000000"
-			coin, _ := coin2.NewCoinFromString(anyAmount)
+			coin, _ := coin.NewCoinFromString(anyAmount)
 
 			_ = coin.Negate()
 			Expect(coin.String()).To(Equal("10000000"))
@@ -219,7 +219,7 @@ var _ = Describe("Coin", func() {
 	Describe("ToBigInt", func() {
 		It("should return copy of the underlying big.Int", func() {
 			anyCoin := "100000000"
-			coin, _ := coin2.NewCoinFromString(anyCoin)
+			coin, _ := coin.NewCoinFromString(anyCoin)
 
 			copiedCoin := coin.ToBigInt()
 			_, _ = copiedCoin.SetString("12345678", 10)
@@ -230,7 +230,7 @@ var _ = Describe("Coin", func() {
 
 	Describe("jsoniter.Marshal", func() {
 		It("should return null when the Coin value is nil", func() {
-			coin := coin2.Coin{}
+			coin := coin.Coin{}
 			result, err := jsoniter.Marshal(coin)
 
 			Expect(err).To(BeNil())
@@ -238,7 +238,7 @@ var _ = Describe("Coin", func() {
 		})
 
 		It("should return the Coin amount in string", func() {
-			coin, _ := coin2.NewCoinFromString("12345678")
+			coin, _ := coin.NewCoinFromString("12345678")
 			result, err := jsoniter.Marshal(coin)
 
 			Expect(err).To(BeNil())
@@ -248,7 +248,7 @@ var _ = Describe("Coin", func() {
 
 	Describe("json.Marshal", func() {
 		It("should return null when the Coin value is nil", func() {
-			coin := coin2.Coin{}
+			coin := coin.Coin{}
 			result, err := json.Marshal(coin)
 
 			Expect(err).To(BeNil())
@@ -256,7 +256,7 @@ var _ = Describe("Coin", func() {
 		})
 
 		It("should return the Coin amount in string", func() {
-			coin, _ := coin2.NewCoinFromString("12345678")
+			coin, _ := coin.NewCoinFromString("12345678")
 			result, err := json.Marshal(coin)
 
 			Expect(err).To(BeNil())
