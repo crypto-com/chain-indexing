5ce438c234224051fcf8c072fab46e59c80268ac
Devashish Dixit <devashishdxt@gmail.com>
2020-12-01 13:35:49 +0800

    Problem: Inefficient total set and increment SQL statements
    Solution: Updated SQL statements for set and increment

--- appinterface/projection/view/total.go
+++ appinterface/projection/view/total.go
@@ -29,131 +29,42 @@ func NewTotal(rdbHandle *rdb.Handle, tableName string) *Total {
 }
 
 func (view *Total) Set(identity string, total int64) error {
-	var err error
-
-	var sql string
-	var sqlArgs []interface{}
-
-	sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Select(
-		"1",
-	).From(
-		view.tableName,
-	).Where(
-		"identity = ?", identity,
-	).ToSql()
+	// Postgres UPSERT statement
+	sql, sqlArgs, err := view.rdbHandle.StmtBuilder.
+		Insert(view.tableName).
+		Columns("identity", "total").
+		Values(identity, total).
+		Suffix("ON CONFLICT (identity) DO UPDATE SET total = EXCLUDED.total").
+		ToSql()
 	if err != nil {
-		return fmt.Errorf("error preparing total selection SQL: %v", err)
+		return fmt.Errorf("error building total insertion sql: %v: %w", err, rdb.ErrBuildSQLStmt)
 	}
-	var placeholder int
-	err = view.rdbHandle.QueryRow(sql, sqlArgs...).Scan(&placeholder)
+
+	_, err = view.rdbHandle.Exec(sql, sqlArgs)
+
 	if err != nil {
-		if !errors.Is(err, rdb.ErrNoRows) {
-			return fmt.Errorf("error scanning total: %v", err)
-		}
-		sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Insert(
-			view.tableName,
-		).Columns(
-			"identity",
-			"total",
-		).Values(identity, total).ToSql()
-		if err != nil {
-			return fmt.Errorf("error building total insertion sql: %v: %w", err, rdb.ErrBuildSQLStmt)
-		}
-
-		var execResult rdb.ExecResult
-		if execResult, err = view.rdbHandle.Exec(sql, sqlArgs...); err != nil {
-			return fmt.Errorf("error inserting total: %v", err)
-		}
-		if execResult.RowsAffected() != 1 {
-			return errors.New("error inserting total: no rows inserted")
-		}
-
-		return nil
-	}
-
-	sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Update(
-		view.tableName,
-	).Set(
-		"total", total,
-	).Where(
-		"identity = ?", identity,
-	).ToSql()
-	if err != nil {
-		return fmt.Errorf("error building total update sql: %v", err)
-	}
-
-	var execResult rdb.ExecResult
-	if execResult, err = view.rdbHandle.Exec(sql, sqlArgs...); err != nil {
-		return fmt.Errorf("error updating total: %v", err)
-	}
-	if execResult.RowsAffected() != 1 {
-		return errors.New("error updating total: no rows updated")
+		return fmt.Errorf("error inserting total: %v: %w", err, rdb.ErrWrite)
 	}
 
 	return nil
 }
 
-func (view *Total) Increment(identity string, incTotal int64) error {
-	var err error
-
-	var sql string
-	var sqlArgs []interface{}
-
-	sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Select(
-		"total",
-	).From(
-		view.tableName,
-	).Where(
-		"identity = ?", identity,
-	).ToSql()
+func (view *Total) Increment(identity string, total int64) error {
+	// Postgres UPSERT statement
+	sql, sqlArgs, err := view.rdbHandle.StmtBuilder.
+		Insert(view.tableName+" AS totals").
+		Columns("identity", "total").
+		Values(identity, total).
+		Suffix("ON CONFLICT (identity) DO UPDATE SET total = totals.total + EXCLUDED.total").
+		ToSql()
 	if err != nil {
-		return fmt.Errorf("error preparing total selection SQL: %v", err)
+		return fmt.Errorf("error building total insertion sql: %v: %w", err, rdb.ErrBuildSQLStmt)
 	}
 
-	var prevTotal int64
-	err = view.rdbHandle.QueryRow(sql, sqlArgs...).Scan(&prevTotal)
+	_, err = view.rdbHandle.Exec(sql, sqlArgs)
+
 	if err != nil {
-		if !errors.Is(err, rdb.ErrNoRows) {
-			return fmt.Errorf("error scanning total of identity: %v", err)
-		}
-		sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Insert(
-			view.tableName,
-		).Columns(
-			"identity",
-			"total",
-		).Values(identity, incTotal).ToSql()
-		if err != nil {
-			return fmt.Errorf("error building total insertion sql: %v: %w", err, rdb.ErrBuildSQLStmt)
-		}
-
-		var execResult rdb.ExecResult
-		if execResult, err = view.rdbHandle.Exec(sql, sqlArgs...); err != nil {
-			return fmt.Errorf("error inserting total: %v", err)
-		}
-		if execResult.RowsAffected() != 1 {
-			return errors.New("error inserting total: no rows inserted")
-		}
-
-		return nil
-	}
-
-	sql, sqlArgs, err = view.rdbHandle.StmtBuilder.Update(
-		view.tableName,
-	).Set(
-		"total", prevTotal+incTotal,
-	).Where(
-		"identity = ?", identity,
-	).ToSql()
-	if err != nil {
-		return fmt.Errorf("error building total update sql: %v", err)
-	}
-
-	var execResult rdb.ExecResult
-	if execResult, err = view.rdbHandle.Exec(sql, sqlArgs...); err != nil {
-		return fmt.Errorf("error updating total: %v", err)
-	}
-	if execResult.RowsAffected() != 1 {
-		return errors.New("error updating total: no rows updated")
+		return fmt.Errorf("error inserting total: %v: %w", err, rdb.ErrWrite)
 	}
 
 	return nil
