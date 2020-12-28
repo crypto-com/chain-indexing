ca5f3c7dafb259dab7a41eb09250c0a27aca5373
hitesh.goel <hitesh.goel@SGMAC0008.local>
2020-12-28 13:08:58 +0800

    - Removed settings table.

--- migrations/20208830152110_crossfire_dryrun_settings.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS crossfire_dryrun_settings;
--- migrations/20208830152110_crossfire_dryrun_settings.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE crossfire_dryrun_settings
-(
-    setting VARCHAR  NOT NULL,
-    value VARCHAR NOT NULL DEFAULT '0',
-    PRIMARY KEY (setting)
-)
-
