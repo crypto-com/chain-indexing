315d38352326c4409402ef763add455ebd7ed7fd
owenz <owen@crypto.com>
2020-12-28 17:13:11 +0800

    feat: add entry for crossfire projection

---
+++ appinterface/projection/crossfire/crossfire.go
@@ -0,0 +1,265 @@
+package crossfire
+
+import (
+	"fmt"
+	"github.com/crypto-com/chain-indexing/appinterface/projection/rdbprojectionbase"
+	"github.com/crypto-com/chain-indexing/appinterface/rdb"
+	event_entity "github.com/crypto-com/chain-indexing/entity/event"
+	applogger "github.com/crypto-com/chain-indexing/internal/logger"
+	"github.com/crypto-com/chain-indexing/internal/utctime"
+	event_usecase "github.com/crypto-com/chain-indexing/usecase/event"
+)
+
+const DO_NOT_MODIFY = "[do-not-modify]"
+
+type Crossfire struct {
+	*rdbprojectionbase.Base
+
+	rdbConn rdb.Conn
+	logger  applogger.Logger
+	conNodeAddressPrefix string
+}
+
+func NewCrossfire(logger applogger.Logger, rdbConn rdb.Conn, conNodeAddressPrefix string) *Crossfire {
+	return &Crossfire{
+		rdbprojectionbase.NewRDbBase(rdbConn.ToHandle(), "Crossfire"),
+
+		rdbConn,
+		logger,
+		conNodeAddressPrefix,
+	}
+}
+
+func (_ *Crossfire) GetEventsToListen() []string {
+	return []string{
+		event_usecase.BLOCK_CREATED,
+
+		event_usecase.MSG_CREATE_VALIDATOR_CREATED,
+		event_usecase.MSG_EDIT_VALIDATOR_CREATED,
+		event_usecase.MSG_DELEGATE_CREATED,
+		event_usecase.MSG_BEGIN_REDELEGATE_CREATED,
+		event_usecase.MSG_UNDELEGATE_CREATED,
+		event_usecase.MSG_WITHDRAW_DELEGATOR_REWARD_CREATED,
+		event_usecase.MSG_WITHDRAW_VALIDATOR_COMMISSION_CREATED,
+		event_usecase.BLOCK_PROPOSER_REWARDED,
+		event_usecase.BLOCK_REWARDED,
+		event_usecase.BLOCK_COMMISSIONED,
+		event_usecase.VALIDATOR_JAILED,
+		event_usecase.VALIDATOR_SLASHED,
+		event_usecase.MSG_UNJAIL_CREATED,
+		event_usecase.POWER_CHANGED,
+	}
+}
+
+func (projection *Crossfire) OnInit() error {
+	return nil
+}
+
+func (projection *Crossfire) HandleEvents(height int64, events []event_entity.Event) error {
+	rdbTx, err := projection.rdbConn.Begin()
+	if err != nil {
+		return fmt.Errorf("error beginning transaction: %v", err)
+	}
+
+	committed := false
+	defer func() {
+		if !committed {
+			_ = rdbTx.Rollback()
+		}
+	}()
+
+	rdbTxHandle := rdbTx.ToHandle()
+	//crossfireValidatorsView := view.NewCrossfireValidators(rdbTxHandle)
+	//crossfireValidatorsView := "foobar"
+
+	var blockTime utctime.UTCTime
+	var blockHash string
+	for _, event := range events {
+		if blockCreatedEvent, ok := event.(*event_usecase.BlockCreated); ok {
+			blockTime = blockCreatedEvent.Block.Time
+			blockHash = blockCreatedEvent.Block.Hash
+		}
+	}
+	// TODO: remove print
+	fmt.Println(blockTime, blockHash)
+
+	// TODO: views preparation starts
+	//if err := projection.projectCrossfireValidatorView(crossfireValidatorsView, height, events); err != nil {
+	//	return fmt.Errorf("error projecting validator view: %v", err)
+	//}
+	// TODO ends: views preparation ends and update current height as handled
+
+	if err := projection.UpdateLastHandledEventHeight(rdbTxHandle, height); err != nil {
+		return fmt.Errorf("error updating last handled event height: %v", err)
+	}
+
+	if err := rdbTx.Commit(); err != nil {
+		return fmt.Errorf("error committing changes: %v", err)
+	}
+	committed = true
+	return nil
+}
+
+// TODO: update with crossfire_validators view
+//func (projection *Crossfire) projectCrossfireValidatorView(
+//	validatorsView *view.Validators,
+//	blockHeight int64,
+//	events []event_entity.Event,
+//) error {
+//	// MsgCreateValidator should be handled first
+//	for _, event := range events {
+//		if msgCreateValidatorEvent, ok := event.(*event_usecase.MsgCreateValidator); ok {
+//			projection.logger.Debug("handling MsgCreateValidator event")
+//
+//			consensusNodeAddress, err := tmcosmosutils.ConsensusNodeAddressFromPubKey(
+//				projection.conNodeAddressPrefix, msgCreateValidatorEvent.Pubkey,
+//			)
+//			if err != nil {
+//				return fmt.Errorf("error converting consensus node pubkey to address: %v", err)
+//			}
+//			validatorRow := view.ValidatorRow{
+//				ConsensusNodeAddress:         consensusNodeAddress,
+//				OperatorAddress:              msgCreateValidatorEvent.ValidatorAddress,
+//				InitialDelegatorAddress:      msgCreateValidatorEvent.DelegatorAddress,
+//				MinSelfDelegation:            msgCreateValidatorEvent.MinSelfDelegation,
+//				Status:                       constants.UNBONDED,
+//				Jailed:                       false,
+//				JoinedAtBlockHeight:          blockHeight,
+//				Power:                        "0",
+//				MaybeUnbondingHeight:         nil,
+//				MaybeUnbondingCompletionTime: nil,
+//				Moniker:                      msgCreateValidatorEvent.Description.Moniker,
+//				Identity:                     msgCreateValidatorEvent.Description.Identity,
+//				Website:                      msgCreateValidatorEvent.Description.Website,
+//				SecurityContact:              msgCreateValidatorEvent.Description.SecurityContact,
+//				Details:                      msgCreateValidatorEvent.Description.Details,
+//				CommissionRate:               msgCreateValidatorEvent.CommissionRates.Rate,
+//				CommissionMaxRate:            msgCreateValidatorEvent.CommissionRates.MaxRate,
+//				CommissionMaxChangeRate:      msgCreateValidatorEvent.CommissionRates.MaxChangeRate,
+//			}
+//
+//			isJoined, joinedAtBlockHeight, err := validatorsView.LastJoinedBlockHeight(
+//				validatorRow.OperatorAddress, validatorRow.ConsensusNodeAddress,
+//			)
+//			if err != nil {
+//				return fmt.Errorf("error querying validator last joined block height: %v", err)
+//			}
+//			if isJoined {
+//				validatorRow.JoinedAtBlockHeight = joinedAtBlockHeight
+//			}
+//
+//			if err := validatorsView.Upsert(&validatorRow); err != nil {
+//				return fmt.Errorf("error inserting new validator into view: %v", err)
+//			}
+//		}
+//	}
+//
+//	for _, event := range events {
+//		if msgEditValidatorEvent, ok := event.(*event_usecase.MsgEditValidator); ok {
+//			projection.logger.Debug("handling MsgEditValidator event")
+//
+//			mutValidatorRow, err := validatorsView.FindBy(view.ValidatorIdentity{
+//				MaybeOperatorAddress: &msgEditValidatorEvent.ValidatorAddress,
+//			})
+//			if err != nil {
+//				return fmt.Errorf(
+//					"error getting existing validator %s from view", msgEditValidatorEvent.ValidatorAddress,
+//				)
+//			}
+//
+//			if msgEditValidatorEvent.Description.Moniker != DO_NOT_MODIFY {
+//				mutValidatorRow.Moniker = msgEditValidatorEvent.Description.Moniker
+//			}
+//			if msgEditValidatorEvent.Description.Identity != DO_NOT_MODIFY {
+//				mutValidatorRow.Identity = msgEditValidatorEvent.Description.Identity
+//			}
+//			if msgEditValidatorEvent.Description.Details != DO_NOT_MODIFY {
+//				mutValidatorRow.Details = msgEditValidatorEvent.Description.Details
+//			}
+//			if msgEditValidatorEvent.Description.SecurityContact != DO_NOT_MODIFY {
+//				mutValidatorRow.SecurityContact = msgEditValidatorEvent.Description.SecurityContact
+//			}
+//			if msgEditValidatorEvent.Description.Website != DO_NOT_MODIFY {
+//				mutValidatorRow.Website = msgEditValidatorEvent.Description.Website
+//			}
+//
+//			if msgEditValidatorEvent.MaybeCommissionRate != nil {
+//				mutValidatorRow.CommissionRate = *msgEditValidatorEvent.MaybeCommissionRate
+//			}
+//			if msgEditValidatorEvent.MaybeMinSelfDelegation != nil {
+//				mutValidatorRow.MinSelfDelegation = *msgEditValidatorEvent.MaybeMinSelfDelegation
+//			}
+//
+//			if err := validatorsView.Update(mutValidatorRow); err != nil {
+//				return fmt.Errorf("error updating validator into view: %v", err)
+//			}
+//		} else if validatorJailedEvent, ok := event.(*event_usecase.ValidatorJailed); ok {
+//			projection.logger.Debug("handling ValidatorJailed event")
+//
+//			mutValidatorRow, err := validatorsView.FindBy(view.ValidatorIdentity{
+//				MaybeConsensusNodeAddress: &validatorJailedEvent.ConsensusNodeAddress,
+//			})
+//			if err != nil {
+//				return fmt.Errorf(
+//					"error getting existing validator `%s` from view", validatorJailedEvent.ConsensusNodeAddress,
+//				)
+//			}
+//
+//			mutValidatorRow.Status = constants.JAILED
+//			mutValidatorRow.Jailed = true
+//
+//			if err := validatorsView.Update(mutValidatorRow); err != nil {
+//				return fmt.Errorf("error updating validator into view: %v", err)
+//			}
+//		} else if msgUnjailEvent, ok := event.(*event_usecase.MsgUnjail); ok {
+//			projection.logger.Debug("handling MsgUnjail event")
+//
+//			mutValidatorRow, err := validatorsView.FindBy(view.ValidatorIdentity{
+//				MaybeOperatorAddress: &msgUnjailEvent.ValidatorAddr,
+//			})
+//			if err != nil {
+//				return fmt.Errorf("error getting existing validator `%s` from view", msgUnjailEvent.ValidatorAddr)
+//			}
+//
+//			mutValidatorRow.Status = constants.BONDED
+//			mutValidatorRow.Jailed = false
+//
+//			if err := validatorsView.Update(mutValidatorRow); err != nil {
+//				return fmt.Errorf("error updating validator into view: %v", err)
+//			}
+//		} else if powerChangedEvent, ok := event.(*event_usecase.PowerChanged); ok {
+//			projection.logger.Debug("handling PowerChange event")
+//
+//			pubkey, convErr := base64.StdEncoding.DecodeString(powerChangedEvent.TendermintPubkey)
+//			if convErr != nil {
+//				return fmt.Errorf("error base64 decoding tendermint pubkey")
+//			}
+//			consensusNodeAddress, convErr := tmcosmosutils.ConsensusNodeAddressFromTmPubKey(
+//				projection.conNodeAddressPrefix, pubkey,
+//			)
+//			if convErr != nil {
+//				return fmt.Errorf("error converting tendermint pubkey to consensus pubkey")
+//			}
+//
+//			mutValidatorRow, err := validatorsView.FindBy(view.ValidatorIdentity{
+//				MaybeConsensusNodeAddress: &consensusNodeAddress,
+//			})
+//			if err != nil {
+//				return fmt.Errorf("error getting existing validator `%s` from view", consensusNodeAddress)
+//			}
+//
+//			mutValidatorRow.Power = powerChangedEvent.Power
+//			if powerChangedEvent.Power == "0" && !mutValidatorRow.Jailed {
+//				mutValidatorRow.Status = constants.UNBONDED
+//			} else if powerChangedEvent.Power != "0" {
+//				mutValidatorRow.Status = constants.BONDED
+//			}
+//
+//			if err := validatorsView.Update(mutValidatorRow); err != nil {
+//				return fmt.Errorf("error updating validator into view: %v", err)
+//			}
+//		}
+//	}
+//
+//	return nil
+//}
---
+++ appinterface/projection/crossfire/crossfire_test.go
@@ -0,0 +1,33 @@
+package crossfire_test
+
+import (
+	"github.com/crypto-com/chain-indexing/appinterface/projection/crossfire"
+	. "github.com/crypto-com/chain-indexing/appinterface/rdb/test"
+	entity_projection "github.com/crypto-com/chain-indexing/entity/projection"
+	"github.com/crypto-com/chain-indexing/infrastructure/pg"
+	. "github.com/crypto-com/chain-indexing/internal/logger/test"
+	. "github.com/crypto-com/chain-indexing/test"
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Block", func() {
+	It("should implement projection", func() {
+		fakeLogger := NewFakeLogger()
+		fakeRdbConn := NewFakeRDbConn()
+		var _ entity_projection.Projection = crossfire.NewCrossfire(fakeLogger, fakeRdbConn, "tcrocnclcons")
+	})
+
+	WithTestPgxConn(func(pgConn *pg.PgxConn, pgMigrate *pg.Migrate) {
+		BeforeEach(func() {
+			_ = pgMigrate.Reset()
+			pgMigrate.MustUp()
+		})
+
+		AfterEach(func() {
+			_ = pgMigrate.Reset()
+		})
+
+		It("should project crossfire_validators view when event is BlockCreated", func() {
+		})
+	})
+})
