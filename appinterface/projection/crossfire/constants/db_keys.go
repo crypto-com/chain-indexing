6da318d9111132bdf7c998ad7820606ef3658b97
hitesh.goel <hitesh.goel@SGMAC0008.local>
2020-12-30 23:29:38 +0800

    Added constants

---
+++ appinterface/projection/crossfire/constants/db_keys.go
@@ -0,0 +1,6 @@
+package constants
+
+const VOTED_PROPOSAL_ID string = "voted_proposal_id"
+const NETWORK_UPGRADE string = "network_upgrade"
+const DB_KEY_SEPARATOR string = ":"
+const TASK_PHASE_2_PROPOSAL_VOTE_COLUMN_NAME = "task_phase_2_proposal_vote"
