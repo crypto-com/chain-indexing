2c0067d9e4893efd17cdc839df15a06c73aeaa05
hitesh.goel <hitesh.goel@SGMAC0008.local>
2021-01-01 06:52:57 +0800

    Test case for TransactionCreated count scenarios

--- appinterface/projection/crossfire/crossfire_test.go
+++ appinterface/projection/crossfire/crossfire_test.go
@@ -434,5 +434,194 @@ var _ = Describe("Crossfire", func() {
 			Expect(voted_proposal_id).To(Equal(int64(14)))
 
 		})
+
+		It("should project crossfire_validators view when event is TransactionCreated", func() {
+			crossfireValidatorStatsView := view.NewCrossfireValidatorsStats(pgConn.ToHandle())
+			crossfireValidatorView := view.NewCrossfireValidators(pgConn.ToHandle())
+
+			anyHeight := int64(1)
+
+			phaseOneBlockCreatedEvent := event_usecase.NewBlockCreated(&usecase_model.Block{
+				Height:          anyHeight,
+				Hash:            "B69554A020537DA8E7C7610A318180C09BFEB91229BB85D4A78DDA2FACF68A48",
+				Time:            utctime.FromUnixNano(int64(1610942500000000000)),
+				AppHash:         "24474D86CBFA7E6328D473C17A9E46CD5A80FFE82A348A74844BF3E2BA2B3AF1",
+				ProposerAddress: "F9E6FFB9B536956201AA138224FD888D03775AB4",
+				Txs: []string{
+					"AAAMZqICtpjLrA3uEe3Rkg6cqDgQl0iBwG1Wm8ORZRzKL9EBAE1R0oP73H8AhHG1E1dke4ppXA/+43AyxWX7oVBTlQGnAABsiqVxCipneyzo9n1t7xauih3rXxssZTChLv/oPHUaSAAAAgAAAAAAAAAAAMw4kh8pZRDC442WIIUIo6+fIE1cTHkwEiv++LwFjNLwNtPTO1AVJx+dGivu9FZK3cN8r0mvLPVunExBxwKqKDkwVUbVSaIejuSc6Wm38rp+ekZjIExhf41FIwkocStPzBY4VYQ31YE7pmNnKocfCEg9CcKH+MIFYJ0FSxoEiMratLXjKAD6B/aMZ3pRCxxL/YxFNs+YKzGbgdKcLtydZ1B/OTGUeNU3SLNQJGmJeTL0AOwvQ4j7KkiQFZGa8uoQQEJo9rXjIg+xPk7a+zBdwC91EY/ZZz7pdVQMUgHBeKgsVvy0o78BOPWfuziBK5xVsoz5K8ZlMYQNPC5mY+bguMKYNJ5PQ9rzn+LseYT5jhalUsQrPABhEFOoQVUH1id3rszDkLLTn2/d89N/JJGY1+mL+upWWAsmJw1yTHqibSJ7RbiGffnh93MCOHeRe5OLrfmDFfhqOLNt9yuMYNdyJ+noVsfI7Ws9Kpxe2SnuCWBs9yOgM0l7UTMuIokqhGCkarXge/DUWLUcV694Jr8qcJT3OtoQohN+p+Xj66nowJLgFW7xBdFsT4vv7xI8giFxUpB+JkLgRZz2d2eam3iLDCHR+sNyvIDuXDUXhKM6aIykGgvHVHr3bBJRy/JPZFC0A3kqmheMnJpbV6kHXaIbmbgeQeXc+wxq1swFVxkwp14zbRmHwSGVGRgihjmoFYl9MyorQzNFETgp28gq2AKrCHNnIRs63m2cD5X4jZaFzfYAv9ifpOKqRtDgIFG0olge/ig00FFL6KdHySg1Qaab7g==",
+				},
+				Signatures: []usecase_model.BlockSignature{
+					{
+						BlockIdFlag:      2,
+						ValidatorAddress: "F9E6FFB9B536956201AA138224FD888D03775AB4",
+						Timestamp:        utctime.FromUnixNano(int64(1000000)),
+						Signature:        "ZW2pUcKFN/oPQCmdCouchXmgpPyd/Ddo45dhHEMwsBeHTBuSJh15zUMmfl5FZsPHeKC8citFvOm/52bgl5XHCw==",
+					},
+					{
+						BlockIdFlag:      2,
+						ValidatorAddress: "031E3891DDB94FC7C7C132B7CD9736738110C889",
+						Timestamp:        utctime.FromUnixNano(int64(2000000)),
+						Signature:        "uhWDC9NDT86FbRVGbOM2lGY8sVkWU51JJ9F8gPwTfK0ebcui1R34oM+jhPKdStn/4sq4qDgzbsN66cQ5kl8NAw==",
+					},
+				},
+			})
+
+			phaseTwoBlockCreatedEvent := event_usecase.NewBlockCreated(&usecase_model.Block{
+				Height:          anyHeight,
+				Hash:            "B69554A020537DA8E7C7610A318180C09BFEB91229BB85D4A78DDA2FACF68A48",
+				Time:            utctime.FromUnixNano(int64(1611547500000000000)),
+				AppHash:         "24474D86CBFA7E6328D473C17A9E46CD5A80FFE82A348A74844BF3E2BA2B3AF1",
+				ProposerAddress: "F9E6FFB9B536956201AA138224FD888D03775AB4",
+				Txs: []string{
+					"AAAMZqICtpjLrA3uEe3Rkg6cqDgQl0iBwG1Wm8ORZRzKL9EBAE1R0oP73H8AhHG1E1dke4ppXA/+43AyxWX7oVBTlQGnAABsiqVxCipneyzo9n1t7xauih3rXxssZTChLv/oPHUaSAAAAgAAAAAAAAAAAMw4kh8pZRDC442WIIUIo6+fIE1cTHkwEiv++LwFjNLwNtPTO1AVJx+dGivu9FZK3cN8r0mvLPVunExBxwKqKDkwVUbVSaIejuSc6Wm38rp+ekZjIExhf41FIwkocStPzBY4VYQ31YE7pmNnKocfCEg9CcKH+MIFYJ0FSxoEiMratLXjKAD6B/aMZ3pRCxxL/YxFNs+YKzGbgdKcLtydZ1B/OTGUeNU3SLNQJGmJeTL0AOwvQ4j7KkiQFZGa8uoQQEJo9rXjIg+xPk7a+zBdwC91EY/ZZz7pdVQMUgHBeKgsVvy0o78BOPWfuziBK5xVsoz5K8ZlMYQNPC5mY+bguMKYNJ5PQ9rzn+LseYT5jhalUsQrPABhEFOoQVUH1id3rszDkLLTn2/d89N/JJGY1+mL+upWWAsmJw1yTHqibSJ7RbiGffnh93MCOHeRe5OLrfmDFfhqOLNt9yuMYNdyJ+noVsfI7Ws9Kpxe2SnuCWBs9yOgM0l7UTMuIokqhGCkarXge/DUWLUcV694Jr8qcJT3OtoQohN+p+Xj66nowJLgFW7xBdFsT4vv7xI8giFxUpB+JkLgRZz2d2eam3iLDCHR+sNyvIDuXDUXhKM6aIykGgvHVHr3bBJRy/JPZFC0A3kqmheMnJpbV6kHXaIbmbgeQeXc+wxq1swFVxkwp14zbRmHwSGVGRgihjmoFYl9MyorQzNFETgp28gq2AKrCHNnIRs63m2cD5X4jZaFzfYAv9ifpOKqRtDgIFG0olge/ig00FFL6KdHySg1Qaab7g==",
+				},
+				Signatures: []usecase_model.BlockSignature{
+					{
+						BlockIdFlag:      2,
+						ValidatorAddress: "F9E6FFB9B536956201AA138224FD888D03775AB4",
+						Timestamp:        utctime.FromUnixNano(int64(1000000)),
+						Signature:        "ZW2pUcKFN/oPQCmdCouchXmgpPyd/Ddo45dhHEMwsBeHTBuSJh15zUMmfl5FZsPHeKC8citFvOm/52bgl5XHCw==",
+					},
+					{
+						BlockIdFlag:      2,
+						ValidatorAddress: "031E3891DDB94FC7C7C132B7CD9736738110C889",
+						Timestamp:        utctime.FromUnixNano(int64(2000000)),
+						Signature:        "uhWDC9NDT86FbRVGbOM2lGY8sVkWU51JJ9F8gPwTfK0ebcui1R34oM+jhPKdStn/4sq4qDgzbsN66cQ5kl8NAw==",
+					},
+				},
+			})
+			txCreatedParams := model.CreateTransactionParams{
+				TxHash:   "A6D4C1F59A9D232747CA4F8A484F1F3B14A0075E801DF2A25F472B4280505B74",
+				Code:     0,
+				Log:      "{\"events\":[]}",
+				MsgCount: 1,
+				Signers: []model.TransactionSigner{
+					{
+						Type:            "/cosmos.crypto.secp256k1.PubKey",
+						Pubkeys:         []string{"tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh"},
+						AccountSequence: uint64(1),
+					},
+					{
+						Type:            "/cosmos.crypto.secp256k1.PubKey",
+						Pubkeys:         []string{"tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh"},
+						AccountSequence: uint64(2),
+					},
+					{
+						Type:            "/cosmos.crypto.secp256k1.PubKey",
+						Pubkeys:         []string{"tcro1khkxmphc7sv0fqrej3rltsslrstud78c0jl6l6"},
+						AccountSequence: uint64(1),
+					},
+					{
+						Type: "/cosmos.crypto.multisig.LegacyAminoPubKey",
+						Pubkeys: []string{
+							"tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+							"tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+							"tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+						},
+						MaybeThreshold:  primptr.Int(2),
+						AccountSequence: uint64(1),
+					},
+				},
+				Fee:           coin.MustNewCoinFromString("1000"),
+				FeePayer:      "tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+				FeeGranter:    "tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+				GasWanted:     200000,
+				GasUsed:       10000,
+				Memo:          "Test memo",
+				TimeoutHeight: int64(10),
+			}
+
+			transactionCreatedEvent := event_usecase.NewTransactionCreated(anyHeight, txCreatedParams)
+
+			validatorCreatedEvent := event_usecase.NewMsgCreateValidator(event_usecase.MsgCommonParams{
+				BlockHeight: anyHeight,
+				TxHash:      "A6D4C1F59A9D232747CA4F8A484F1F3B14A0075E801DF2A25F472B4280505B74",
+				TxSuccess:   true,
+				MsgIndex:    0,
+			}, usecase_model.MsgCreateValidatorParams{
+				Description: usecase_model.MsgValidatorDescription{
+					Moniker:         "Testing",
+					Identity:        "foo",
+					Website:         "www.example.com",
+					SecurityContact: "foo@example.com",
+					Details:         "example",
+				},
+				Commission: usecase_model.MsgValidatorCommission{
+					Rate:          "0.100000000000000000",
+					MaxRate:       "0.200000000000000000",
+					MaxChangeRate: "0.010000000000000000",
+				},
+				MinSelfDelegation: "1",
+				DelegatorAddress:  "tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh",
+				ValidatorAddress:  "tcrocncl14m5a4kxt2e82uqqs5gtqza29dm5wqzyalddug5",
+				TendermintPubkey:  "na51D8RmKXyWrid9I6wtdxgP6f1Nl3EyNNEzqxVquoM=",
+				Amount:            coin.MustNewCoinFromString("100"),
+			})
+
+			fakeLogger := NewFakeLogger()
+			projection := crossfire.NewCrossfire(
+				fakeLogger,
+				pgConn,
+				"tcrocnclcons",
+				"tcrocncl",
+				1610942400000000000,
+				1611547200000000000,
+				1612756800000000000,
+				1613361599000000000,
+				"tcro15grftg88l0gdw4mg9t9pwnl0pde2asjzvfpkp4",
+				"14",
+			)
+
+			// Fire both events
+			err := projection.HandleEvents(anyHeight, []event_entity.Event{phaseOneBlockCreatedEvent, validatorCreatedEvent, transactionCreatedEvent})
+			errHandleNewBlock := projection.HandleEvents(anyHeight, []event_entity.Event{phaseTwoBlockCreatedEvent, transactionCreatedEvent})
+			Expect(err).To(BeNil())
+			Expect(errHandleNewBlock).To(BeNil())
+
+			//Check validators view count
+			validatorViewCount, err := crossfireValidatorView.Count()
+			Expect(err).To(BeNil())
+			Expect(validatorViewCount).To(Equal(int64(1)))
+
+			//check Validator status
+			crossfireValidatorList, err := crossfireValidatorView.List()
+			Expect(err).To(BeNil())
+			Expect(crossfireValidatorList).To(HaveLen(1))
+			Expect(crossfireValidatorList[0].TaskPhase1NodeSetup).To(Equal(constants.COMPLETED))
+
+			// total count for both address
+			totalCountByAddress1, errTotalCountAddress1 := crossfireValidatorStatsView.FindBy("total_tx_sent:tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh")
+			totalCountByAddress2, errTotalCountAddress2 := crossfireValidatorStatsView.FindBy("total_tx_sent:tcro1khkxmphc7sv0fqrej3rltsslrstud78c0jl6l6")
+
+			// Phase One Count for both address
+			phaseOneCountByAddress1, errPhaseOneCountAddress1 := crossfireValidatorStatsView.FindBy("phase_1_tx_sent:tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh")
+			phaseOneCountByAddress2, errPhaseOneCountAddress2 := crossfireValidatorStatsView.FindBy("phase_1_tx_sent:tcro1khkxmphc7sv0fqrej3rltsslrstud78c0jl6l6")
+
+			// Phase Two Count for both address
+			phaseTwoCountByAddress1, errPhaseTwoCountAddress1 := crossfireValidatorStatsView.FindBy("phase_2_tx_sent:tcro14m5a4kxt2e82uqqs5gtqza29dm5wqzya2jw9sh")
+			phaseTwoCountByAddress2, errPhaseTwoCountAddress2 := crossfireValidatorStatsView.FindBy("phase_2_tx_sent:tcro1khkxmphc7sv0fqrej3rltsslrstud78c0jl6l6")
+
+			Expect(errTotalCountAddress1).To(BeNil())
+			Expect(errTotalCountAddress2).To(BeNil())
+
+			Expect(errPhaseOneCountAddress1).To(BeNil())
+			Expect(errPhaseOneCountAddress2).To(BeNil())
+
+			Expect(errPhaseTwoCountAddress1).To(BeNil())
+			Expect(errPhaseTwoCountAddress2).To(BeNil())
+
+			//Total Count checks
+			Expect(totalCountByAddress1).To(Equal(int64(4)))
+			Expect(totalCountByAddress2).To(Equal(int64(2)))
+
+			//Phase One Count checks
+			Expect(phaseOneCountByAddress1).To(Equal(int64(2)))
+			Expect(phaseOneCountByAddress2).To(Equal(int64(1)))
+
+			//Phase Two Count checks
+			Expect(phaseTwoCountByAddress1).To(Equal(int64(2)))
+			Expect(phaseTwoCountByAddress2).To(Equal(int64(1)))
+
+		})
 	})
 })
