4caa8db96976ca1d52c8347b6000ad36ed19a244
hitesh.goel <hitesh.goel@SGMAC0008.local>
2021-01-06 04:10:16 +0800

    Reinstated the test script

--- test.sh
+++ test.sh
@@ -85,9 +85,7 @@ is_postgres_ready() {
 
 run_test() {
     set +e
-    # ginkgo -r
-    ginkgo -r appinterface/projection/crossfire
-    # ginkgo -r internal/tmcosmosutils
+    ginkgo -r
     RET_VALUE=$?
     set -e
 }
