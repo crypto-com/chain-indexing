624a46b5fc1a248173d96b9ac77ba39614107817
Calvin Lau <calvin@mona.co>
2021-08-16 23:26:40 +0800

    Problem: pgmigrate.sh failed to install migrate under Golang v1.15 or below
    Solution: (Fix #468) Update script to install migrate based on Golang version

--- pgmigrate.sh
+++ pgmigrate.sh
@@ -22,11 +22,74 @@ DB_DRIVER_URL="${DB_PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PO
 
 echoerr() { echo "$@" 1>&2; }
 
+GOLANG_MIGRATE_VERSION="v4.14.1"
 check_migrate() {
     set +e
     command -v migrate > /dev/null
-    RET_VALUE=$?
+    if [[ $? != 0 ]]; then
+        RET_VALUE="NOT_FOUND"
+        set -e
+        return
+    fi
+    VERSION=$(migrate --version 2>&1)
+    if [[ "${VERSION}" != "${GOLANG_MIGRATE_VERSION}" ]]; then
+        if [[ $# == 1 && "$1" == "--verbose" ]]; then
+            echo "WARNING: System is running migrate version ${VERSION}. For best compatibility, we recommended using ${GOLANG_MIGRATE_VERSION}. Consider provide \`--install-dependency\` to try to install this version."
+        fi
+        RET_VALUE="VERSION_MISMATCH"
+        set -e
+        return
+    fi
+    RET_VALUE="OK"
     set -e
+}
+
+is_golang_1_16_and_above() {
+    IFS=$' '
+    GOVERSION="$(go version | { read _ _ v _; echo ${v#go}; })"
+    IFS=$'\n\t'
+    echo ${GOVERSION}
+    MAJOR="$(echo ${GOVERSION} | cut -d. -f1)"
+    MINOR="$(echo ${GOVERSION} | cut -d. -f2)"
+    REVISION="$(echo ${GOVERSION} | cut -d. -f3)"
+    if [[ "${MAJOR}" == "" ]]; then
+      echoerr "Golang version not found. Make sure it is installed."
+      exit 1
+    fi
+
+    if [[ $MAJOR != 1 ]]; then
+      echoerr "Only Golang v1 is supported. Found v${GOVERSION}"
+      exit 1
+    fi
+
+    if [[ $MINOR -ge 16 ]]; then
+      RET_VALUE=1
+    else
+      RET_VALUE=0
+    fi
+}
+
+# https://github.com/golang-migrate/migrate/tree/master/cmd/migrate#versioned
+install_golang_migrate_golang_1_15_and_below() {
+    set -x
+    # https://github.com/golang-migrate/migrate/pull/257#issuecomment-705249902
+    GO111MODULE=off go get -tags 'postgres' -u -d github.com/golang-migrate/migrate/cmd/migrate/
+    cd "${GOPATH}/src/github.com/golang-migrate/migrate/cmd/migrate"
+    git checkout ${GOLANG_MIGRATE_VERSION}
+    go build -tags 'postgres' -ldflags="-X main.Version=$(git describe --tags)" -o "${GOPATH}/bin/migrate" "${GOPATH}/src/github.com/golang-migrate/migrate/cmd/migrate"
+    set +x
+}
+
+install_golang_migrate_universal() {
+    install_golang_migrate_golang_1_15_and_below
+}
+
+install_golang_migrate_golang_1_16_and_above() {
+    # This is the recommended way to install golang-migrate. But the binary installed using this method will always
+    # return `dev` when running `migrate --version` and will confuse the `migrate` version check.
+    set -x
+    go install -tags 'postgres' "github.com/golang-migrate/migrate/v4/cmd/migrate@${GOLANG_MIGRATE_VERSION}"
+    set +x
 }
 
 run_migrate() {
@@ -58,18 +121,27 @@ while [[ $# > 0 ]]; do
     esac
 done
 
-check_migrate
-if [[ "${RET_VALUE}" != 0 ]]; then
-    if [[ "${INSTALL_DEPENDENCY}" = 0 ]]; then
-        echoerr "Cannot find migrate. Provide \`--install-dependency\` to attempt to install it."
-        exit 1
-    fi
-    
-    go get -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate
-    check_migrate
-    if [[ "${RET_VALUE}" != 0 ]]; then
-        echoerr "Failed to install migrate. Please install it manually."
-        exit 1
+check_migrate --verbose
+if [[ "${RET_VALUE}" != "OK" ]]; then
+    if [[ "${INSTALL_DEPENDENCY}" == 0 ]]; then
+        if [[ "${RET_VALUE}" == "NOT_FOUND" ]]; then
+            echoerr "Cannot find migrate. Provide \`--install-dependency\` to try to install it."
+            exit 1
+        fi
+    else
+        is_golang_1_16_and_above
+        if [[ "${RET_VALUE}" == 1 ]]; then
+#            install_golang_migrate_golang_1_16_and_above
+            install_golang_migrate_universal
+        else
+            install_golang_migrate_golang_1_15_and_below
+        fi
+
+        check_migrate
+        if [[ "${RET_VALUE}" == "NOT_FOUND" ]]; then
+            echoerr "Failed to install migrate. Please install it manually."
+            exit 1
+        fi
     fi
 fi
 
@@ -88,7 +160,7 @@ while [[ $# > 0 ]]; do
             run_migrate -source "file://${DB_MIGRATIONS_FOLDER}" -database "${DB_DRIVER_URL}" "${ARGS}" "$@"
         ;;
         version)
-            run_migrate version
+            run_migrate --version
         ;;
         help)
             run_migrate "${ARGS}" help
