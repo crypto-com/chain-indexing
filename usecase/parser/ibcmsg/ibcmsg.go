0a7e54e838888759e0799aeea32d8bf08357dcf6
davcrypto <david.so@crypto.com>
2021-08-27 11:20:12 +0800

    FIX syntax

--- usecase/parser/ibcmsg/ibcmsg.go
+++ usecase/parser/ibcmsg/ibcmsg.go
@@ -868,11 +868,11 @@ func ParseMsgAcknowledgement(
 		MaybeFungibleTokenPacketData: &ibc_model.MsgAcknowledgementFungibleTokenPacketData{
 			FungibleTokenPacketData: rawFungibleTokenPacketData,
 			// https://github.com/cosmos/ibc-go/blob/e98838612a4fa5d240e392aad3409db5ec428f50/modules/apps/transfer/module.go#L327
-			Success: fungibleTokenPacketEvent.HasAttribute("success")
-			&& bytes.Equal(
-				[]byte(fungibleTokenPacketEvent.MustGetAttributeByKey("success")),
-				[]byte{byte(1)},
-			),
+			Success: fungibleTokenPacketEvent.HasAttribute("success") &&
+				bytes.Equal(
+					[]byte(fungibleTokenPacketEvent.MustGetAttributeByKey("success")),
+					[]byte{byte(1)},
+				),
 			Acknowledgement: fungibleTokenPacketEvent.MustGetAttributeByKey("acknowledgement"),
 		},
 
