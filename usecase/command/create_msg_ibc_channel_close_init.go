1debb1ab6e870054132c1f8de9308750760dfff3
Yang Song <yang.song@crypto.com>
2021-10-13 18:13:25 +0800

    Supported parsing MsgChannelCloseInit and MsgChannelCloseConfirm, added test cases

---
+++ usecase/command/create_msg_ibc_channel_close_init.go
@@ -0,0 +1,35 @@
+package command
+
+import (
+	entity_event "github.com/crypto-com/chain-indexing/entity/event"
+	"github.com/crypto-com/chain-indexing/usecase/event"
+	ibc_model "github.com/crypto-com/chain-indexing/usecase/model/ibc"
+)
+
+type CreateMsgIBCChannelCloseInit struct {
+	msgCommonParams event.MsgCommonParams
+	params          ibc_model.MsgChannelCloseInitParams
+}
+
+func NewCreateMsgIBCChannelCloseInit(
+	msgCommonParams event.MsgCommonParams,
+	params ibc_model.MsgChannelCloseInitParams,
+) *CreateMsgIBCChannelCloseInit {
+	return &CreateMsgIBCChannelCloseInit{
+		msgCommonParams,
+		params,
+	}
+}
+
+func (*CreateMsgIBCChannelCloseInit) Name() string {
+	return "CreateMsgIBCChannelCloseInit"
+}
+
+func (*CreateMsgIBCChannelCloseInit) Version() int {
+	return 1
+}
+
+func (cmd *CreateMsgIBCChannelCloseInit) Exec() (entity_event.Event, error) {
+	event := event.NewMsgIBCChannelCloseInit(cmd.msgCommonParams, cmd.params)
+	return event, nil
+}
