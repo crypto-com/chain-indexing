11dd1fa851b19bacfb8d51e417a0c12f893f68bb
Calvin Lau <calvin@mona.co>
2021-11-14 01:30:03 +0800

    Problem: MsgAcknowledgement could be relayed multiple times
    Solution: (Fix #611) Handle MsgAcknowledgement properly

--- usecase/parser/msg_ibc_acknowledgement_test.go
+++ usecase/parser/msg_ibc_acknowledgement_test.go
@@ -4,21 +4,20 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/crypto-com/chain-indexing/external/json"
-	"github.com/crypto-com/chain-indexing/usecase/parser/utils"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/crypto-com/chain-indexing/external/json"
 	"github.com/crypto-com/chain-indexing/infrastructure/tendermint"
 	"github.com/crypto-com/chain-indexing/usecase/event"
 	"github.com/crypto-com/chain-indexing/usecase/parser"
 	usecase_parser_test "github.com/crypto-com/chain-indexing/usecase/parser/test"
+	"github.com/crypto-com/chain-indexing/usecase/parser/utils"
 )
 
 var _ = Describe("ParseMsgCommands", func() {
-	Describe("MsgIBCRecvPacket", func() {
-		It("should parse Msg commands when there is MsgIBCRecvPacket in the transaction", func() {
+	Describe("MsgIBCAcknowledgement", func() {
+		It("should parse Msg commands when there is MsgIBCAcknowledgement in the transaction", func() {
 			expected := `{
   "name": "MsgAcknowledgementCreated",
   "version": 1,
@@ -58,7 +57,7 @@ var _ = Describe("ParseMsgCommands", func() {
       "amount": "1234",
       "success": true,
       "acknowledgement": "{0xc0038ae7a0}",
-			"error": null
+	  "error": null
     },
 
     "packetSequence": "1",
@@ -109,7 +108,7 @@ var _ = Describe("ParseMsgCommands", func() {
 			))
 		})
 
-		It("should parse Msg commands when there is MsgIBCRecvPacket in the transaction, and MsgIBCRecvPacket.maybeMsgTransfer.Error is NOT empty string", func() {
+		It("should parse Msg commands when there is MsgIBCAcknowledgement in the transaction, and MsgIBCAcknowledgement.maybeMsgTransfer.Error is NOT empty string", func() {
 			expected := `{
 	"name": "MsgAcknowledgementCreated",
 	"version": 1,
@@ -211,5 +210,161 @@ var _ = Describe("ParseMsgCommands", func() {
 
 			Expect(json.MustMarshalToString(typedEvent)).To(Equal(expectedWithAcknowledgement))
 		})
+
+		It("should return two MsgIBCAcknowledgement with different success values when there are two MsgAcknowledgement of same packet sequence in the block, and in the second messages the fungible_token_packet event is missing", func() {
+			expectedFirstMsgAck := `{
+	"name": "MsgAcknowledgementCreated",
+	"version": 1,
+	"height": 68821,
+	"uuid": "{UUID}",
+	"msgName": "MsgAcknowledgement",
+	"txHash": "0F1205B05DDDC0B848AD50FE35335C993FEDE7EAD161F2099818B0A03321B97F",
+	"msgIndex": 0,
+	"params": {
+		"packet": {
+			"sequence": "933",
+			"sourcePort": "transfer",
+			"sourceChannel": "channel-0",
+			"destinationPort": "transfer",
+			"destinationChannel": "channel-44",
+			"data": "eyJhbW91bnQiOiI5OTc5Mzk2NzUwMCIsImRlbm9tIjoidHJhbnNmZXIvY2hhbm5lbC0wL2Jhc2Vjcm8iLCJyZWNlaXZlciI6ImNybzFkOWozOHdka2Rwd2Y2MjN1emw3czlucXcwOHVwMzBjd2t2MmNtbCIsInNlbmRlciI6ImNyYzF6ZGFlcHE5d3NraGY1bDV2MGoyd20waHI0a3VjanV3NXllbDI1dSJ9",
+			"timeoutHeight": {
+				"revisionNumber": "0",
+				"revisionHeight": "0"
+			},
+			"timeoutTimestamp": "1636802327974618671"
+		},
+		"acknowledgement": "eyJyZXN1bHQiOiJBUT09In0=",
+		"proofAcked": "CvIHCu8HCjVhY2tzL3BvcnRzL3RyYW5zZmVyL2NoYW5uZWxzL2NoYW5uZWwtNDQvc2VxdWVuY2VzLzkzMxIgCPdVftUYJv4Y2EUSvyTsdQAe268hI6R333KgqfNkCnwaDggBGAEgASoGAAKcxYQDIi4IARIHAgScxYQDIBohIDwQccgiMyrjLbcO8vPDN9d3axcaYEUa1R5jTbc9QrEUIiwIARIoBAicxYQDIJGWtfcKpISRSjNnTDmj7yfu2sYN7ylgVs8CsQmS+vZoICIsCAESKAYQnMWEAyDqIfPb8+Y9G25V4zTBgfYqEJpDH+14RXV3JjrTW+df2CAiLggBEgcIHJzFhAMgGiEgEH2fJo7kkzfUAdGGInk3u8gQSzsa+NQrQrmn5bWewbsiLAgBEigKLpzFhAMgxvf+3tGUbTd2hmvjvEipzpRg9PAN/Kx4HnX7qa/QGs8gIiwIARIoDFycxYQDIAkoKqt5WEDBrKES7vGpFkzWy4hi3L4VwZWlVFwrC1e8ICItCAESKQ7EAZzFhAMg9FqcFtOC9laytmdPUwKyyNoiwVuctCR6zxHQMR6jXl0gIi0IARIpEM4CnMWEAyAPR1g6VGLnyuFCnjd0Qj/JwbSQeKfVAovu+orlmkp3kyAiLQgBEikS1AOcxYQDILefyRWK+YSCuiVB3I2wwMDqVp16Sgyzz7ZmN75Lv3MMICItCAESKRTqB5zFhAMgDug2QuDrvM0oN2IDF1XJKr1XjxykD3SJR3/pVeDiIVwgIi0IARIpFooQnMWEAyCf5HTrSGpUSnJt9shB6Fo7yc/jM7XJUNRBNIRmypMelCAiLQgBEikY3BycxYQDIKFRwO1Q3eK2suvY0jiTVX6wKO7+ET6k3hC+e5+8pIBAICItCAESKRq+NJzFhAMgt8C/50Q0KYN5cW5MvvbnIS30/SLIyr+FO+/1ndgeiMcgIi0IARIpHOhfnMWEAyAHXT2SdgE8GcynTSUMRHBSTlvhiCGZjVuiTc55LvKI3SAiLggBEioepqUBnMWEAyDDwDasfVEFnpB16dEB6oTx3NuiDde72z8+wrGFgIEgXCAiLggBEioglsYCnMWEAyB9ye9kzIq5x74bFoFth70N2lCRzVyE7nzMN+h/ZRLDNyAiMAgBEgki4J4EnMWEAyAaISAKSYySjL8NwUPrqNv1B6piWwT2UGi0hO/RU9FC4hA/dyIwCAESCSSMlgicxYQDIBohIIHy7B0D/O1whzgbJ5YyCqC9BTiVEh4OQPdVpEwEGV65IjAIARIJKKLME5zFhAMgGiEgaxhG9Cvrw3YI2vEuL5aX7dF0gHsG+BrIbIzCXuddjg0K/AEK+QEKA2liYxIg+YaXZdDxHjzJJk0pV2Gpd0ZC96RM5nwKb+NMKozNbNMaCQgBGAEgASoBACIlCAESIQHGFYo+eJC7BDuwbQgF9JWTxmomsU5OToBz+rgm3rPoyiIlCAESIQFNau0HEtyE0UZQMBEACzeDPjN7NGmlnudP7Bj5/JnIVSIlCAESIQGwRJ6T3arUBmLdqgJBi0plcbY2gFLfK4mgRtbDp5+0/SInCAESAQEaIFzyRJ+I9dxVu3tUnJky7PZCHnVdfz2qDyAyPf+Oa1CBIicIARIBARog0gxBfMi3VGo2ZD71VCYvXMTGeOt/tSI6qNkOvMmZtdA=",
+		"proofHeight": {
+			"revisionNumber": "1",
+			"revisionHeight": "3182927"
+		},
+		"signer": "crc1yzl6cnq3f66ew24d7u97vmp45nkckhwg4ak8hl",
+		"application": "transfer",
+		"messageType": "MsgTransfer",
+		"maybeMsgTransfer": {
+			"sender": "crc1zdaepq9wskhf5l5v0j2wm0hr4kucjuw5yel25u",
+			"receiver": "cro1d9j38wdkdpwf623uzl7s9nqw08up30cwkv2cml",
+			"denom": "transfer/channel-0/basecro",
+			"amount": "99793967500",
+			"success": true,
+			"acknowledgement": "{ACKNOWLEDGEMENT}",
+			"error": null
+		},
+		"packetSequence": "933",
+		"channelOrdering": "ORDER_UNORDERED",
+		"connectionId": "connection-0"
+	}		
+}
+`
+
+			expectedSecondMsgAck := `{
+	"name": "MsgAcknowledgementCreated",
+	"version": 1,
+	"height": 68821,
+	"uuid": "{UUID}",
+	"msgName": "MsgAcknowledgement",
+	"txHash": "961A752199E991F5653FB8773DF6A20F1381C3C7B3FAD2D817988BE6640FCF4E",
+	"msgIndex": 1,
+	"params": {
+		"packet": {
+			"sequence": "933",
+			"sourcePort": "transfer",
+			"sourceChannel": "channel-0",
+			"destinationPort": "transfer",
+			"destinationChannel": "channel-44",
+			"data": "eyJhbW91bnQiOiI5OTc5Mzk2NzUwMCIsImRlbm9tIjoidHJhbnNmZXIvY2hhbm5lbC0wL2Jhc2Vjcm8iLCJyZWNlaXZlciI6ImNybzFkOWozOHdka2Rwd2Y2MjN1emw3czlucXcwOHVwMzBjd2t2MmNtbCIsInNlbmRlciI6ImNyYzF6ZGFlcHE5d3NraGY1bDV2MGoyd20waHI0a3VjanV3NXllbDI1dSJ9",
+			"timeoutHeight": {
+				"revisionNumber": "0",
+				"revisionHeight": "0"
+			},
+			"timeoutTimestamp": "1636802327974618671"
+		},
+		"acknowledgement": "eyJyZXN1bHQiOiJBUT09In0=",
+		"proofAcked": "CvIHCu8HCjVhY2tzL3BvcnRzL3RyYW5zZmVyL2NoYW5uZWxzL2NoYW5uZWwtNDQvc2VxdWVuY2VzLzkzMxIgCPdVftUYJv4Y2EUSvyTsdQAe268hI6R333KgqfNkCnwaDggBGAEgASoGAAKcxYQDIi4IARIHAgScxYQDIBohIDwQccgiMyrjLbcO8vPDN9d3axcaYEUa1R5jTbc9QrEUIiwIARIoBAicxYQDIJGWtfcKpISRSjNnTDmj7yfu2sYN7ylgVs8CsQmS+vZoICIsCAESKAYQnMWEAyDqIfPb8+Y9G25V4zTBgfYqEJpDH+14RXV3JjrTW+df2CAiLggBEgcIHJzFhAMgGiEgEH2fJo7kkzfUAdGGInk3u8gQSzsa+NQrQrmn5bWewbsiLAgBEigKLpzFhAMgxvf+3tGUbTd2hmvjvEipzpRg9PAN/Kx4HnX7qa/QGs8gIiwIARIoDFycxYQDIAkoKqt5WEDBrKES7vGpFkzWy4hi3L4VwZWlVFwrC1e8ICItCAESKQ7EAZzFhAMg9FqcFtOC9laytmdPUwKyyNoiwVuctCR6zxHQMR6jXl0gIi0IARIpEM4CnMWEAyAPR1g6VGLnyuFCnjd0Qj/JwbSQeKfVAovu+orlmkp3kyAiLQgBEikS1AOcxYQDILefyRWK+YSCuiVB3I2wwMDqVp16Sgyzz7ZmN75Lv3MMICItCAESKRTqB5zFhAMgDug2QuDrvM0oN2IDF1XJKr1XjxykD3SJR3/pVeDiIVwgIi0IARIpFooQnMWEAyCf5HTrSGpUSnJt9shB6Fo7yc/jM7XJUNRBNIRmypMelCAiLQgBEikY3BycxYQDIKFRwO1Q3eK2suvY0jiTVX6wKO7+ET6k3hC+e5+8pIBAICItCAESKRq+NJzFhAMgt8C/50Q0KYN5cW5MvvbnIS30/SLIyr+FO+/1ndgeiMcgIi0IARIpHOhfnMWEAyAHXT2SdgE8GcynTSUMRHBSTlvhiCGZjVuiTc55LvKI3SAiLggBEioepqUBnMWEAyDDwDasfVEFnpB16dEB6oTx3NuiDde72z8+wrGFgIEgXCAiLggBEioglsYCnMWEAyB9ye9kzIq5x74bFoFth70N2lCRzVyE7nzMN+h/ZRLDNyAiMAgBEgki4J4EnMWEAyAaISAKSYySjL8NwUPrqNv1B6piWwT2UGi0hO/RU9FC4hA/dyIwCAESCSSMlgicxYQDIBohIIHy7B0D/O1whzgbJ5YyCqC9BTiVEh4OQPdVpEwEGV65IjAIARIJKKLME5zFhAMgGiEgaxhG9Cvrw3YI2vEuL5aX7dF0gHsG+BrIbIzCXuddjg0K/AEK+QEKA2liYxIg+YaXZdDxHjzJJk0pV2Gpd0ZC96RM5nwKb+NMKozNbNMaCQgBGAEgASoBACIlCAESIQHGFYo+eJC7BDuwbQgF9JWTxmomsU5OToBz+rgm3rPoyiIlCAESIQFNau0HEtyE0UZQMBEACzeDPjN7NGmlnudP7Bj5/JnIVSIlCAESIQGwRJ6T3arUBmLdqgJBi0plcbY2gFLfK4mgRtbDp5+0/SInCAESAQEaIFzyRJ+I9dxVu3tUnJky7PZCHnVdfz2qDyAyPf+Oa1CBIicIARIBARog0gxBfMi3VGo2ZD71VCYvXMTGeOt/tSI6qNkOvMmZtdA=",
+		"proofHeight": {
+			"revisionNumber": "1",
+			"revisionHeight": "3182927"
+		},
+		"signer": "crc1aaxs058pksrq8cx3k0nrxv60p2a9c7nq527949",
+		"application": "transfer",
+		"messageType": "MsgTransfer",
+		"maybeMsgTransfer": {
+			"sender": "crc1zdaepq9wskhf5l5v0j2wm0hr4kucjuw5yel25u",
+			"receiver": "cro1d9j38wdkdpwf623uzl7s9nqw08up30cwkv2cml",
+			"denom": "transfer/channel-0/basecro",
+			"amount": "99793967500",
+			"success": false,
+			"acknowledgement": "",
+			"error": null
+		},
+		"packetSequence": "933",
+		"channelOrdering": "ORDER_UNORDERED",
+		"connectionId": "connection-0"
+	}		
+}
+`
+
+			txDecoder := utils.NewTxDecoder()
+			block, _, _ := tendermint.ParseBlockResp(strings.NewReader(
+				usecase_parser_test.TX_MSG_ACKNOWLEDGEMENT_DUPLICATE_PACKET_SEQUENCE_BLOCK_RESP,
+			))
+			blockResults, _ := tendermint.ParseBlockResultsResp(strings.NewReader(
+				usecase_parser_test.TX_MSG_ACKNOWLEDGEMENT_DUPLICATE_PACKET_SEQUENCE_BLOCK_RESULTS_RESP,
+			))
+
+			accountAddressPrefix := "crc"
+			stakingDenom := "basecro"
+
+			pm := usecase_parser_test.InitParserManager()
+
+			cmds, err := parser.ParseBlockTxsMsgToCommands(
+				pm,
+				txDecoder,
+				block,
+				blockResults,
+				accountAddressPrefix,
+				stakingDenom,
+			)
+			Expect(err).To(BeNil())
+			Expect(cmds).To(HaveLen(4))
+			Expect(cmds[0].Name()).To(Equal("CreateMsgIBCUpdateClient"))
+			Expect(cmds[1].Name()).To(Equal("CreateMsgIBCAcknowledgement"))
+			Expect(cmds[2].Name()).To(Equal("CreateMsgIBCUpdateClient"))
+			Expect(cmds[3].Name()).To(Equal("CreateMsgIBCAcknowledgement"))
+
+			firstMsgAckCmd := cmds[1]
+			secondMsgAckCmd := cmds[3]
+
+			regex, _ := regexp.Compile("\n?\r?\\s?")
+
+			untypedFirstMsgAckEvent, _ := firstMsgAckCmd.Exec()
+			typedFistMsgAckEvent := untypedFirstMsgAckEvent.(*event.MsgIBCAcknowledgement)
+			expectedFirstMsgAckWithUUID := strings.Replace(
+				regex.ReplaceAllString(expectedFirstMsgAck, ""),
+				"{UUID}",
+				typedFistMsgAckEvent.UUID(),
+				-1,
+			)
+			expectedFirstMsgAckWithAcknowledgement := strings.Replace(
+				expectedFirstMsgAckWithUUID,
+				"{ACKNOWLEDGEMENT}",
+				"result:\\\"\\\\001\\\" ",
+				-1,
+			)
+			Expect(json.MustMarshalToString(typedFistMsgAckEvent)).To(Equal(expectedFirstMsgAckWithAcknowledgement))
+
+			untypedSecondMsgAckEvent, _ := secondMsgAckCmd.Exec()
+			typedSecondMsgAckCmd := untypedSecondMsgAckEvent.(*event.MsgIBCAcknowledgement)
+			expectedSecondMsgAckWithUUID := strings.Replace(
+				regex.ReplaceAllString(expectedSecondMsgAck, ""),
+				"{UUID}",
+				typedSecondMsgAckCmd.UUID(),
+				-1,
+			)
+			Expect(json.MustMarshalToString(typedSecondMsgAckCmd)).To(Equal(expectedSecondMsgAckWithUUID))
+		})
 	})
+
 })
