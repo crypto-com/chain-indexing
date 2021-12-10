7cc3a0cd06dd8c31fb6a5d9cf5e3e64159b3c1d5
davcrypto <david.so@crypto.com>
2021-12-10 14:04:24 +0800

    ADD console log

--- infrastructure/metric/prometheus/http.go
+++ infrastructure/metric/prometheus/http.go
@@ -1,12 +1,14 @@
 package prometheus
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Run(path, port string) {
+	fmt.Println("prometheus server running")
 	http.Handle(path, promhttp.Handler())
 	http.ListenAndServe(":"+port, nil)
 }
