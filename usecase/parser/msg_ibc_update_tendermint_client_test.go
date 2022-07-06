a49ac8ba6fcac9fde7e6a915c7984842af766350
vincentysc <vincent.ys.chan@crypto.com>
2022-07-06 13:49:20 +0800

    fix indentation

--- usecase/parser/msg_ibc_update_tendermint_client_test.go
+++ usecase/parser/msg_ibc_update_tendermint_client_test.go
@@ -153,7 +153,7 @@ var _ = Describe("ParseMsgCommands", func() {
 
 			cmds, err := parser.ParseBlockTxsMsgToCommands(
 				pm,
-        logger,
+				logger,
 				txDecoder,
 				block,
 				blockResults,
