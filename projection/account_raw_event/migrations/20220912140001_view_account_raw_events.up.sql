25038913c3dc1183e0cfe70cc80d7fc640c5cc53
vincentysc <vincent.ys.chan@crypto.com>
2022-09-15 10:40:48 +0800

    update migration

--- projection/account_raw_event/migrations/20220912140001_view_account_raw_events.up.sql
+++ projection/account_raw_event/migrations/20220912140001_view_account_raw_events.up.sql
@@ -12,4 +12,4 @@ create index view_account_raw_events_account_btree_index
     on view_account_raw_events (account);
 
 create index view_account_raw_events_block_hash_btree_index
-    on view_raw_block_events (block_hash);
+    on view_account_raw_events (block_hash);
