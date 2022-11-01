c9fe6e935b7b555d11a62b7ee2118ea6885f833e
vincentysc <vincent.ys.chan@crypto.com>
2022-11-01 12:53:52 +0800

    update drop constraint name

--- projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.down.sql
+++ projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE view_nft_tokens ADD CONSTRAINT view_nft_tokens_ids_btree_index UNIQUE (denom_id, token_id);
-
-DROP INDEX if exists view_nft_tokens_denom_id_token_id_btree_index;
+ALTER TABLE view_nft_tokens ADD CONSTRAINT view_nft_tokens_denom_id_token_id_key UNIQUE (denom_id, token_id);
--- projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.up.sql
+++ projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.up.sql
@@ -1,3 +1,1 @@
-ALTER TABLE view_nft_tokens DROP CONSTRAINT view_nft_tokens_ids_btree_index;
-
-CREATE INDEX view_nft_tokens_denom_id_token_id_btree_index ON view_nft_tokens USING btree (denom_id, token_id);
+ALTER TABLE view_nft_tokens DROP CONSTRAINT view_nft_tokens_denom_id_token_id_key;
