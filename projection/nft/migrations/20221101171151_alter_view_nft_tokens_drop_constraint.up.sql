4514b5196e26850ef286f246b423d5e070632073
vincentysc <vincent.ys.chan@crypto.com>
2022-11-01 12:42:21 +0800

    remove nft token table unique

--- projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.up.sql
+++ projection/nft/migrations/20221101171151_alter_view_nft_tokens_drop_constraint.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE view_nft_tokens DROP CONSTRAINT view_nft_tokens_denom_id_token_id_key;
 
-CREATE UNIQUE INDEX view_nft_tokens_denom_id_token_id_btree_index ON view_nft_tokens USING btree (denom_id, token_id);
+CREATE INDEX view_nft_tokens_denom_id_token_id_btree_index ON view_nft_tokens USING btree (denom_id, token_id);
