52122f8e8f66260ba93c99a75a3230217c7698a8
davidso <david.so@crypto.com>
2022-11-02 17:40:28 +0800

    FIX

--- bootstrap/syncmanager.go
+++ bootstrap/syncmanager.go
@@ -263,9 +263,9 @@ func (manager *SyncManager) syncBlockWorker(blockHeight int64) ([]command_entity
 				} else {
 					tx.Tx = resTx.Tx
 				}
+			} else {
+				tx.Tx = *decodedTx
 			}
-
-			tx.Tx = *decodedTx
 		} else {
 			var resTx *model.Tx
 			resTx, err = manager.cosmosClient.Tx(txHash)
@@ -274,25 +274,6 @@ func (manager *SyncManager) syncBlockWorker(blockHeight int64) ([]command_entity
 			} else {
 				tx.Tx = resTx.Tx
 			}
-		}
-		txs = append(txs, *tx)
-
-		var resTx *model.Tx
-		resTx, err = manager.cosmosClient.Tx(txHash)
-		if err != nil {
-			if manager.txDecoder != nil {
-				var decodedTx *model.CosmosTx
-				decodedTx, err = manager.txDecoder.DecodeBase64(txHex)
-				if err != nil {
-					return nil, fmt.Errorf("error decoding chain txs (%s) at height %d: %v", txHex, blockHeight, err)
-				}
-
-				tx.Tx = *decodedTx
-			} else {
-				return nil, fmt.Errorf("error requesting chain txs (%s) at height %d: %v", txHex, blockHeight, err)
-			}
-		} else {
-			tx.Tx = resTx.Tx
 		}
 		txs = append(txs, *tx)
 	}
