09b10223a73d4ec201385fd447b4d2a754ab4a5b
davidso <david.so@crypto.com>
2023-02-22 11:56:10 +0800

    ADD: health check endpoint

---
+++ infrastructure/httpapi/handlers/health.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	applogger "github.com/crypto-com/chain-indexing/external/logger"
+
+	status_polling "github.com/crypto-com/chain-indexing/appinterface/polling"
+	"github.com/crypto-com/chain-indexing/appinterface/rdb"
+	"github.com/crypto-com/chain-indexing/infrastructure/httpapi"
+	"github.com/valyala/fasthttp"
+)
+
+type HealthHandler struct {
+	logger applogger.Logger
+
+	statusView *status_polling.Status
+}
+
+func NewHealthHandler(logger applogger.Logger, rdbHandle *rdb.Handle) *HealthHandler {
+	return &HealthHandler{
+		logger.WithFields(applogger.LogFields{
+			"module": "StatusHandler",
+		}),
+		status_polling.NewStatus(rdbHandle),
+	}
+}
+
+func (handler *HealthHandler) HealthCheck(ctx *fasthttp.RequestCtx) {
+	_, err := handler.statusView.FindBy("LatestHeight")
+	if err != nil {
+		handler.logger.Errorf("error fetching latest height: %v", err)
+		httpapi.InternalServerError(ctx)
+		return
+	}
+
+	httpapi.Success(ctx, "OK")
+}
