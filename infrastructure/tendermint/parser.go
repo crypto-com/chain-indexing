46c847d82b52e5a73b24071e35ee8b919607fb0a
Calvin Lau <calvin@crypto.com>
2023-05-10 14:22:51 +0800

    Revert TODO comment

--- infrastructure/tendermint/parser.go
+++ infrastructure/tendermint/parser.go
@@ -16,8 +16,6 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-// TODO: change parser to accept []byte instead of io.Reader
-
 // Block related parsing functions
 func ParseGenesisResp(rawRespReader io.Reader, strictParsing bool) (*genesis.Genesis, error) {
 	var genesisResp GenesisResp
