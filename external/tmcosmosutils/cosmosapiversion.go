aa79fdd166b11277f4247e77579cae8cc20e5264
Vincent YS Chan <vincent.ys.chan@crypto.com>
2026-01-30 14:06:46 +0800

    lint fix

--- external/tmcosmosutils/cosmosapiversion.go
+++ external/tmcosmosutils/cosmosapiversion.go
@@ -14,10 +14,8 @@ var CosmosAPIVersionMap = map[string]bool{
 	CosmosAPIVersionV1:      true,
 }
 
-// MustNewCoinFromAmountInterface returns a Coin from the amount map in the form of
-// map[string]interface{}. It panics when the amount is invalid.
 func GetCosmosAPIVersionByMsgType(msgType string) string {
-	cosmosAPIVersion := "v1beta1"
+	cosmosAPIVersion := DefaultCosmosAPIVersion
 	parts := strings.Split(msgType, ".")
 	if len(parts) >= 2 {
 		cosmosAPIVersion = parts[len(parts)-2]
